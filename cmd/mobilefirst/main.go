9c80b6528e0f980bb0b989be1498e5c8a2cd77a0
Peny <43066291+pee331@users.noreply.github.com>
2025-10-19 12:23:17 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/mobilefirst/main.go
@@ -0,0 +1,23 @@
+// cmd/mobilefirst/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "mobilefirst/internal/mobilefirst"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := mobilefirst.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
